c17516f36bd519b73807a6b4a13950304ffbbb85
Jimi Jukkala <jukkalajimi@gmail.com>
2023-04-12 23:02:16 +0300

    Added the warning window and validator if user tries to change the values to illegal value

--- src/main/java/com/example/harkkatyo/LisaaController.java
+++ src/main/java/com/example/harkkatyo/LisaaController.java
@@ -31,7 +31,7 @@ public class LisaaController {
     private Button peruutaLisaaNappi, lisaaNappi;
 
     @FXML
-    private TextField uusiFeidi, uusiKunto, uusiLiito, uusiMalli, uusiNopeus, uusiPaino, uusiVakaus, uusiValmistaja, uusiVuosi;
+    private TextField Feidi, Kunto, Liito, Malli, Nopeus, Paino, Vakaus, Valmistaja, Vuosi;
 
     @FXML
     private ChoiceBox<String> uusiMuovi, uusiVari;
@@ -44,15 +44,15 @@ public class LisaaController {
         String tussiValinta = tussiJoo.isSelected() ? "Kyllä" : "Ei";
         String erikoiseraValinta = erikoiseraJoo.isSelected() ? "Kyllä" : "Ei";
         return new String[] {
-                uusiValmistaja.getText(),   // 0
-                uusiMalli.getText(),        // 1
-                uusiVuosi.getText(),        // 2
-                uusiPaino.getText(),        // 3
-                uusiNopeus.getText(),       // 4
-                uusiLiito.getText(),        // 5
-                uusiVakaus.getText(),       // 6
-                uusiFeidi.getText(),        // 7
-                uusiKunto.getText(),        // 8
+                Valmistaja.getText(),   // 0
+                Malli.getText(),        // 1
+                Vuosi.getText(),        // 2
+                Paino.getText(),        // 3
+                Nopeus.getText(),       // 4
+                Liito.getText(),        // 5
+                Vakaus.getText(),       // 6
+                Feidi.getText(),        // 7
+                Kunto.getText(),        // 8
                 tussiValinta,               // 9
                 erikoiseraValinta,          // 10
                 uusiMuovi.getValue(),       // 11
@@ -64,32 +64,92 @@ public class LisaaController {
         return tallennetaan;
     }
 
+
+    public void varoitusIkkuna(String teksti) {
+        // Sanotaan käyttäjälle, ettei tätä arvoa voi antaa kiekolle
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(teksti);
+        alert.showAndWait();
+    }
+
+
+    public boolean tarkastaTekstikenttaNumeerinen(int min, int max, TextField text) {
+        // TODO mitä jos tekstikenttä on tyhjä?
+
+        int arvo;
+        try {
+            arvo = Integer.parseInt(text.getText());
+        } catch (NumberFormatException e) {
+            text.setStyle("-fx-text-inner-color: red;");
+            varoitusIkkuna("Antamasi arvo kentälle " + text.getId() + " ei ole kokonaisluku");
+            return false;
+        }
+
+        if (arvo >= min && arvo <= max) {
+            text.setStyle("-fx-text-inner-color: black;");
+            return true;
+        }
+
+        text.setStyle("-fx-text-inner-color: red;");
+        varoitusIkkuna("Antamasi arvo kentälle " + text.getId() + " ei ole välillä " + min + " ja " + max);
+        return false;
+    }
+
+
+    public boolean tarkastaTekstikenttaMerkkijono(int max, TextField text) {
+        if (text.getText().length() <= max) {
+            text.setStyle("-fx-text-inner-color: black;");
+            return true;
+        }
+
+        text.setStyle("-fx-text-inner-color: red;");
+        varoitusIkkuna("Antamasi arvo kentälle " + text.getId() + " on yli sallitun merkkimäärän: " + max);
+        return false;
+    }
+
+
     @FXML
     void lisaaDataa(ActionEvent event) {
 
-        String tussiValinta = tussiJoo.isSelected() ? "Kyllä" : "Ei";
-        String erikoiseraValinta = erikoiseraJoo.isSelected() ? "Kyllä" : "Ei";
+        // TODO mitä vittua tää on
 
-        String[] tiedot = {
-                uusiValmistaja.getText(),   // 0
-                uusiMalli.getText(),        // 1
-                uusiVuosi.getText(),        // 2
-                uusiPaino.getText(),        // 3
-                uusiNopeus.getText(),       // 4
-                uusiLiito.getText(),        // 5
-                uusiVakaus.getText(),       // 6
-                uusiFeidi.getText(),        // 7
-                uusiKunto.getText(),        // 8
-                tussiValinta,               // 9
-                erikoiseraValinta,          // 10
-                uusiMuovi.getValue(),       // 11
-                uusiVari.getValue(),        // 12
-        };
+        boolean valmistajaOK = tarkastaTekstikenttaMerkkijono(15, Valmistaja);
+        boolean malliOK = tarkastaTekstikenttaMerkkijono(15, Malli);
+        boolean vuosiOK = tarkastaTekstikenttaNumeerinen(1980, 2023, Vuosi);
+        boolean painoOK = tarkastaTekstikenttaNumeerinen(130, 200, Paino);
+        boolean nopeusOK = tarkastaTekstikenttaNumeerinen(1, 15, Nopeus);
+        boolean liitoOK = tarkastaTekstikenttaNumeerinen(0, 7, Liito);
+        boolean vakausOK = tarkastaTekstikenttaNumeerinen(-4, 2, Vakaus);
+        boolean feidiOK = tarkastaTekstikenttaNumeerinen(0, 7, Feidi);
+        boolean kuntoOK = tarkastaTekstikenttaNumeerinen(0, 10, Kunto);
 
-        tallennetaan = true;
-        // Lähetä
-        Stage stage = (Stage) lisaaNappi.getScene().getWindow();
-        stage.close();
+        if (
+            valmistajaOK && malliOK && vuosiOK && painoOK && nopeusOK && liitoOK && vakausOK && feidiOK && kuntoOK
+        ) {
+            String tussiValinta = tussiJoo.isSelected() ? "Kyllä" : "Ei";
+            String erikoiseraValinta = erikoiseraJoo.isSelected() ? "Kyllä" : "Ei";
+
+            String[] tiedot = {
+                    Valmistaja.getText(),   // 0
+                    Malli.getText(),        // 1
+                    Vuosi.getText(),        // 2
+                    Paino.getText(),        // 3
+                    Nopeus.getText(),       // 4
+                    Liito.getText(),        // 5
+                    Vakaus.getText(),       // 6
+                    Feidi.getText(),        // 7
+                    Kunto.getText(),        // 8
+                    tussiValinta,               // 9
+                    erikoiseraValinta,          // 10
+                    uusiMuovi.getValue(),       // 11
+                    uusiVari.getValue(),        // 12
+            };
+
+            tallennetaan = true;
+            // Lähetä
+            Stage stage = (Stage) lisaaNappi.getScene().getWindow();
+            stage.close();
+        }
     }
 
     @FXML
