c8df3925271ac0a1321fa629133a40a5643d7abd
Jimi Jukkala <jukkalajimi@gmail.com>
2023-04-12 23:03:53 +0300

    Removed useless code

--- src/main/java/com/example/harkkatyo/Suodatin.java
+++ src/main/java/com/example/harkkatyo/Suodatin.java
@@ -1,72 +1,23 @@
 package com.example.harkkatyo;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Suodatin {
-    protected Kiekko kiekko;
-    private ArrayList<Kenno> kennot = new ArrayList<>();
+    private int[][] numeeriset;
+    private String[] merkkijonot;
 
     public Suodatin() {}
 
-    public void lisaaNumeerinenSuodatin(int min, int max, int arvo) {
-        kennot.add(new Kenno(min, max, arvo));
-    }
-    public void lisaaMerkkijonoSuodatin(String haluttu, String arvo) {
-        kennot.add(new Kenno(haluttu, arvo));
-    }
-
     public boolean tarkastaKiekko(Kiekko kiekko) {
-        this.kiekko = kiekko;
-        for (Kenno kenno: kennot) {
-
-        }
+        System.out.println(Arrays.toString(numeeriset[0]));
         return true;
     }
 
-    public void tyhjennaSuodatin() { kennot.clear(); }
+    public void asetaNumeerisetSuodattimet(int[][] suodattimet) {
+        numeeriset = suodattimet;
+    }
 
-    static class Kenno{
-        private String viesti;
-
-        // Jos verrataan merkkijonoja
-        private String haluttu = null;
-        private String merkkijonoArvo;
-
-        // Jos verrataan numeerisia arvoja
-        private int min;
-        private int max;
-        private int arvo;
-
-        public Kenno(String arvo, String haluttu) {
-            merkkijonoArvo = arvo;
-            this.haluttu = haluttu;
-        }
-
-        public Kenno(int min, int max, int arvo) {
-            this.min = min;
-            this.max = max;
-            this.arvo = arvo;
-        }
-
-        public void setViesti(String viesti) {
-            this.viesti = viesti;
-        }
-
-        public String getViesti() {
-            return viesti;
-        }
-
-        public boolean suodata() {
-            if (haluttu == null ) {
-                return testInteger();
-            } else {
-                return testString();
-            }
-        }
-
-        private boolean testInteger() { return arvo >= min && arvo <= max; }
-
-        private boolean testString() { return merkkijonoArvo.equals(haluttu);}
+    public void asetaValikkoSuodattimet(String[] merkkijonot) {
 
     }
+
 }
