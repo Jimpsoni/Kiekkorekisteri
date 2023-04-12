bb5b18136cd166480155538142358a4b6ead3da2
Jimi Jukkala <jukkalajimi@gmail.com>
2023-04-12 23:00:48 +0300

    Reformat code and changes the datatype of vuosi, paino and kunto to integers

--- src/main/java/com/example/harkkatyo/Kiekko.java
+++ src/main/java/com/example/harkkatyo/Kiekko.java
@@ -1,21 +1,9 @@
 package com.example.harkkatyo;
 
 public class Kiekko {
-    private String ID;
-    private String vari;
-    private String kunto;
-    private String vuosimalli;
-    private String paino;
+    private String ID, vari, tussia, erikoisera, valmistaja, malli;
 
-    private String tussia;
-    private String erikoisera;
-
-    private int muoviID;
-    private int valmistajaID;
-
-    private String valmistaja;
-    private String malli;
-
+    private int kunto, vuosimalli, paino, muoviID, valmistajaID;
 
 
     public void kiekko() {}
@@ -28,10 +16,10 @@ public class Kiekko {
         ID = merkkijono[0];
         valmistajaID = Integer.parseInt(merkkijono[1]);
         vari = merkkijono[2];
-        vuosimalli = merkkijono[3];
-        paino = merkkijono[4];
+        vuosimalli = Integer.parseInt(merkkijono[3]);
+        paino = Integer.parseInt(merkkijono[4]);
         muoviID = Integer.parseInt(merkkijono[5]);
-        kunto = merkkijono[6];
+        kunto = Integer.parseInt(merkkijono[6]);
         tussia = merkkijono[7];
         erikoisera = merkkijono[8];
     }
@@ -39,13 +27,6 @@ public class Kiekko {
 
     public String toString() {
         return "#" + ID + " " +  valmistaja + ", " + malli;
-    }
-
-    /**
-     * @return palauttaa tärkeimmät tiedot yhdessä merkkijonotaulukossa
-     */
-    public String[] getInformation() {
-        return new String[] {vari, vuosimalli, paino, kunto, tussia, erikoisera};
     }
 
     public int getID() { return Integer.parseInt(this.ID); }
@@ -59,15 +40,15 @@ public class Kiekko {
     public String getValmistaja() { return valmistaja; }
     public String getMalli() { return malli; }
 
-    public String getValmistusvuosi() { return vuosimalli; }
-    public String getPaino() { return paino; }
-    public String getKunto() { return kunto; }
+    public int getValmistusvuosi() { return vuosimalli; }
+    public int getPaino() { return paino; }
+    public int getKunto() { return kunto; }
     public String getVari() { return vari; }
     public String getTussit() { return tussia; }
     public String getErikois() { return erikoisera; }
-    public void setPaino(String paino) { this.paino = paino; }
-    public void setValmistusvuosi(String vuosi) { this.vuosimalli = vuosi; }
-    public void setKunto(String kunto) { this.kunto = kunto; }
+    public void setPaino(int paino) { this.paino = paino; }
+    public void setValmistusvuosi(int vuosi) { this.vuosimalli = vuosi; }
+    public void setKunto(int kunto) { this.kunto = kunto; }
     public void setTussit(String tussia) {this.tussia = tussia; }
     public void setErikoisera(String erikoisera) {this.erikoisera = erikoisera;}
     public void setMuovi(int ID) {muoviID = ID;}
