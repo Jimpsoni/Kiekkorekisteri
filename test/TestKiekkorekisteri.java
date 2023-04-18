dd63a67d5957b4ad6487f3e39ed3f478f9786ac5
Jimi Jukkala <jukkalajimi@gmail.com>
2023-04-18 16:51:21 +0300

    Added tests

--- test/TestKiekkorekisteri.java
+++ test/TestKiekkorekisteri.java
@@ -3,11 +3,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestKiekkorekisteri {
+    Kiekkorekisteri kr = new Kiekkorekisteri("testKiekot.dat", "testValmistajaJaMalli.dat", "testMuovi.dat");
+
     @Test
-    public void test() {
-        // TODO lue testi tiedostoja, eikä tuotanto tiedostoja
-        Kiekkorekisteri kr = new Kiekkorekisteri();
-
-
+    public void testgetMuovit() {
+        Assertions.assertArrayEquals(new String[] {"C-line", "Champion", "soft", "jawbreaker"}, kr.getMuovit());
     }
+
+    @Test
+    public void testGetMuoviByID() {
+        Assertions.assertEquals("jawbreaker", kr.getMuoviByID(4));
+        Assertions.assertEquals("C-line", kr.getMuoviByID(1));
+    }
+
+    @Test
+    public void testSuodata() {
+        Assertions.assertEquals(1, kr.suodata("^disc*").length);
+        Assertions.assertEquals(1, kr.suodata("^inn*").length);
+        Assertions.assertEquals(2, kr.suodata("^*").length);
+    }
+
+
 }
